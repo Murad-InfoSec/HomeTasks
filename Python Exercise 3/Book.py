9468fd39cc0a402ad170490bf4488af879ffb44b
Murad <56511804+Murad-InfoSec@users.noreply.github.com>
2026-01-18 21:03:16 +0400

    Update Book.py

--- Python Exercise 3/Book.py
+++ Python Exercise 3/Book.py
@@ -1,1 +1,23 @@
 
+
+class Book:
+    def __init__(self,name,author,pages,ISBN):
+        self.name=name
+        self.author=author        
+        self.pages=pages
+        self.ISBN=ISBN
+
+    def book_info(self):    
+        print(f"Name: \"{self.name}\" Author: {self.author} Pages: {self.pages}")
+        print(f"{self.ISBN}")
+
+    def __str__(self) -> str:   
+        print(f"Name: \"{self.name}\" Author: {self.author} Pages: {self.pages}")
+        print(f"ISBN: {self.ISBN}")
+
+g_ourell_1984=Book("1984","George Ourell",334,"978-0451524935")
+g_ourell_1984.book_info()
+    
+
+        
+
